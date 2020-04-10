980cd8d0e18eaa2df58ac8d07574722ba54ce7e4
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-04-10 09:54:29 +0200

    feat(pages): add navigation between pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,9 @@ import React from "react"
 export default () => (
   <div>
     <h1>This is the about page</h1>
+    <div>
+      <a href="/">Home</a> | <a href="/about">About me</a>
+    </div>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quasi iure
       error officiis unde? Modi, non. Molestiae suscipit atque hic reiciendis ut
