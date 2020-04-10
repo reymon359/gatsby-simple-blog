d795111c63629e4e6d6df75a2b726a1dd5880253
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-04-10 11:07:54 +0200

    feat(components/layout): create a Layout component and use it in the pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 export default () => (
-  <div>
-    <h1>This is the index page</h1>
+  <Layout>
     <div>
-      <Link to="/">Home</Link> | <Link to="/about">About me</Link>
+      <h1>This is the index page</h1>
+      <div>
+        <Link to="/">Home</Link> | <Link to="/about">About me</Link>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quasi iure
+        error officiis unde? Modi, non. Molestiae suscipit atque hic reiciendis
+        ut architecto, dolorem consectetur adipisci perspiciatis repudiandae?
+        Molestiae, ullam!
+      </p>
     </div>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quasi iure
-      error officiis unde? Modi, non. Molestiae suscipit atque hic reiciendis ut
-      architecto, dolorem consectetur adipisci perspiciatis repudiandae?
-      Molestiae, ullam!
-    </p>
-  </div>
+  </Layout>
 )
