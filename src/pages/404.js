d7777f9d99db03e2a0a7e59f429413b0dfbf7d9a
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-04-10 10:13:22 +0200

    style(pages 404): add custom styles with CSS modules to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,11 @@
 import React from "react"
+import styles from "./404.module.css"
 
 export default () => (
   <div>
-    <h1>404. Page not found</h1>
-    <p>The page you are looking for does not exist</p>
+    <h1 className={styles.header}>404. Page not found</h1>
+    <p className={styles.errorMessage}>
+      The page you are looking for does not exist
+    </p>
   </div>
 )
