1e90debcbc2464b0c7089b6d0f41136db9baa222
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-04-10 11:29:30 +0200

    feat(components/header): add HomeButton and SocialButtons to header

--- src/components/header.js
+++ src/components/header.js
@@ -9,10 +9,43 @@ const HeaderLink = props => (
   </Link>
 )
 
+// HomeButton component
+const HomeButton = props => (
+  <Link to={props.to}>
+    <div className={styles.button}>{props.text}</div>
+  </Link>
+)
+
+// SocialButton component
+const SocialButton = props => {
+  let style = ""
+  let url = ""
+
+  if (props.site === "twitter") {
+    style = styles.buttonTwitter
+    url = "https://twitter.com/" + props.username
+  } else if (props.site === "linkedin") {
+    style = styles.buttonLinkedin
+    url = "https://www.linkedin.com/in/" + props.username
+  } else if (props.site === "github") {
+    style = styles.buttonGithub
+    url = "https://www.github.com/" + props.username
+  }
+
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <div className={style}>{props.children}&nbsp;</div>
+    </a>
+  )
+}
+
 export default () => (
   <header className={styles.container}>
     <div className={styles.row}>
-      <Link to="/">My Gatsby blog</Link>
+      <HomeButton to="/" text="My Gatsby blog" />
+      <SocialButton site="github" username="reymon359"></SocialButton>
+      <SocialButton site="linkedin" username="ramon-morcillo"></SocialButton>
+      <SocialButton site="twitter" username="reymon359"></SocialButton>
     </div>
 
     <div className={styles.row}>
