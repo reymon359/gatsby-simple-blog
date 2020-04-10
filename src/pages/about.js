e0dc41d1c2285e5217088be1e3cf44878a5424f2
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-04-10 11:39:44 +0200

    feat(components/title): create title component & add it to index & about pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import Title from "../components/title"
 
 export default () => (
   <Layout>
+    <Title text="About Me" />
     <div>
-      <h1>This is the about page</h1>
-      <div>
-        <Link to="/">Home</Link> | <Link to="/about">About me</Link>
-      </div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quasi iure
-        error officiis unde? Modi, non. Molestiae suscipit atque hic reiciendis
-        ut architecto, dolorem consectetur adipisci perspiciatis repudiandae?
-        Molestiae, ullam!
-      </p>
+      <Link to="/">Home</Link> | <Link to="/about">About me</Link>
     </div>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quasi iure
+      error officiis unde? Modi, non. Molestiae suscipit atque hic reiciendis ut
+      architecto, dolorem consectetur adipisci perspiciatis repudiandae?
+      Molestiae, ullam!
+    </p>
   </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import Title from "../components/title"
 
 export default () => (
   <Layout>
+    <Title text="Welcome" />
     <div>
-      <h1>This is the index page</h1>
-      <div>
-        <Link to="/">Home</Link> | <Link to="/about">About me</Link>
-      </div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quasi iure
-        error officiis unde? Modi, non. Molestiae suscipit atque hic reiciendis
-        ut architecto, dolorem consectetur adipisci perspiciatis repudiandae?
-        Molestiae, ullam!
-      </p>
+      <Link to="/">Home</Link> | <Link to="/about">About me</Link>
     </div>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quasi iure
+      error officiis unde? Modi, non. Molestiae suscipit atque hic reiciendis ut
+      architecto, dolorem consectetur adipisci perspiciatis repudiandae?
+      Molestiae, ullam!
+    </p>
   </Layout>
 )
