22c37f6161e0220a57997e791275e3eeb5668906
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-04-10 22:45:03 +0200

    fix(pages/about.js): remove Home & About Links from about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,10 @@
-import React from "react"
-import { Link } from "gatsby"
-import Layout from "../components/layout"
-import Title from "../components/title"
+import React from 'react'
+import Layout from '../components/layout'
+import Title from '../components/title'
 
 export default () => (
   <Layout>
     <Title text="About Me" />
-    <div>
-      <Link to="/">Home</Link> | <Link to="/about">About me</Link>
-    </div>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quasi iure
       error officiis unde? Modi, non. Molestiae suscipit atque hic reiciendis ut
