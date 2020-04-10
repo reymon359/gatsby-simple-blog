7dd1547061db5fb3770e4d2af2d7b96c2862c767
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-04-10 13:37:42 +0200

    feat(pages/index.js): fetch and display articles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import React from "react"
-import { Link } from "gatsby"
-import Layout from "../components/layout"
-import Title from "../components/title"
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import Title from '../components/title'
 
-export default () => (
+export default ({ data }) => (
   <Layout>
     <Title text="Welcome" />
     <div>
@@ -15,5 +15,35 @@ export default () => (
       architecto, dolorem consectetur adipisci perspiciatis repudiandae?
       Molestiae, ullam!
     </p>
+    <div>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <article>
+          <div>
+            <strong>{node.frontmatter.title}</strong>
+          </div>
+          <div>{node.excerpt}</div>
+        </article>
+      ))}
+    </div>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            image
+            keywords
+            date(formatString: "MMMM YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
