a225fd9f0a3d897922cafede5ec4b7db1e96384c
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-04-10 11:36:49 +0200

    feat(components/footer): create footer and add to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,12 @@
 import React from "react"
 import styles from "./layout.module.scss"
 import Header from "./header.js"
+import Footer from "./footer.js"
 
 export default ({ children }) => (
   <div className={styles.container}>
     <Header />
-    {children}
+    <div className={styles.content}>{children}</div>
+    <Footer>My new Gatsby Blog {new Date().getFullYear()}</Footer>
   </div>
 )
