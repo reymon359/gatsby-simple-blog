de18c317520e30fcb19f3e1cff4897ae229b906a
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-04-10 10:00:08 +0200

    feat(pages): improve navigation with the use of Gatsby's Link

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default () => (
   <div>
     <h1>This is the about page</h1>
     <div>
-      <a href="/">Home</a> | <a href="/about">About me</a>
+      <Link to="/">Home</Link> | <Link to="/about">About me</Link>
     </div>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quasi iure
